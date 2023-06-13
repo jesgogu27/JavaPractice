15612a80b22ed89ba1f06ce7a6eb0cf030cec9ff
jesgogu27 <jesgogu@hotmail.com>
2023-06-13 07:57:52 -0500

    practica notas

--- Notas.java
+++ Notas.java
@@ -1,7 +1,7 @@
 public class Notas {
   public static void main (String args[]){
     int notaMatematicas = 5;
-    int notaBiologia = 8;
+    int notaBiologia = 2;
     int notaQuimica = 7;
     int notaFinal= 0;
 
@@ -9,11 +9,11 @@ public class Notas {
 
     if(notaFinal >= 6)
       {
-         System.out.println("Aprobo, la nota final fue de: " + notaFinal);
+         System.out.println("\n Aprobo, la nota final fue de: " + notaFinal);
       }
     else
       {
-         System.out.println("Usted Reprobó. Su nota final es de:" + notaFinal +"/n");
+         System.out.println(" \n Usted Reprobo. Su nota final es de:" + notaFinal +"\n");
       }
  }
 }
